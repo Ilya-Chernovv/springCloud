260b6a7a4356471cf280fba3a7544a214c9e344a
Ilya-Chernovv <ilacernov547@gmail.com>
2023-05-05 01:38:08 +0300

    .

--- entities-service/src/main/java/EntityController.java
+++ entities-service/src/main/java/EntityController.java
@@ -3,6 +3,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -11,12 +13,18 @@ public class EntityController {
     private final EntityRepository entityRepository;
     private final RedisSessionService redisSessionService;
 
+
+    // обрабатыват get запросы на получение всех записей текущего пользователя
+    // вызывает getSessionUser для получения текущего пользователя
+    // использует EntityRepository для получения всех записей, принадлежащих этому пользователю
     @GetMapping("/entities")
     public Flux<Entity> getAllEntities(ServerWebExchange exchange) {
         return getSessionUser(exchange)
                 .flatMapMany(user -> entityRepository.findAllByUserId(user.getId()));
     }
 
+    // получает идентификатор сессии из cookies
+    // использует RedisSessionService для получения текущего пользователя по этому идентификатору
     private Mono<User> getSessionUser(ServerWebExchange exchange) {
         String sessionId = exchange.getRequest().getCookies().getFirst("sessionId").getValue();
         return redisSessionService.getUserBySessionId(sessionId);
--- entities-service/src/main/java/EntityRepository.java
+++ entities-service/src/main/java/EntityRepository.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Flux;
 
 @Repository
 public interface EntityRepository extends ReactiveCrudRepository<Entity, Long> {
-
+    // поиск сущностей в базе связанных с конкретным пользователем
     Flux<Entity> findAllByUserId(Long userId);
 
 }
