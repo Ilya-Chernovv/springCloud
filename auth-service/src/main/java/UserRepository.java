e770e41b8fd46abee83ebe26f475eee9c9392be9
Ilya-Chernovv <ilacernov547@gmail.com>
2023-05-05 16:00:44 +0300

    .

--- auth-service/src/main/java/UserRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-    // поиск пользователя в базе по логину
-    Mono<User> findByLogin(String login);
-}
